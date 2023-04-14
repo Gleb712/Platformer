4489a60bb8aac9ba817cf872ecc4e3109648957e
Gleb712 <129591109+Gleb712@users.noreply.github.com>
2023-04-15 00:59:09 +0500

    Add files via upload

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Runtime.CompilerServices;
 
 namespace Platformer
 {
@@ -9,16 +10,29 @@ namespace Platformer
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Texture2D _backgroundSprite;
+        private Rectangle _backgroundRect;
+
+        private Texture2D _playerSprite;
+        private Vector2 _playerPosition;
+        private float _playerSpeed;
+
+        private Texture2D _platformSprite;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
+
+            _graphics.PreferredBackBufferWidth = 1200;
+            _graphics.PreferredBackBufferHeight = 800;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _playerPosition = new Vector2(350, 600);
+            _playerSpeed = 5f;
 
             base.Initialize();
         }
@@ -27,7 +41,11 @@ namespace Platformer
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            _playerSprite = Content.Load<Texture2D>("player");
+            _platformSprite = Content.Load<Texture2D>("platform");
+
+            _backgroundSprite = Content.Load<Texture2D>("background");
+            _backgroundRect = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,7 +53,29 @@ namespace Platformer
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            // Move player left
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                _playerPosition.X -= _playerSpeed;
+            }
+
+            // Move player right
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                _playerPosition.X += _playerSpeed;
+            }
+
+            // Move player up
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                _playerPosition.Y -= _playerSpeed;
+            }
+
+            // Move player down
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                _playerPosition.Y += _playerSpeed;
+            }
 
             base.Update(gameTime);
         }
@@ -44,7 +84,13 @@ namespace Platformer
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_backgroundSprite, _backgroundRect, Color.White);
+            _spriteBatch.End();
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_playerSprite, _playerPosition, Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
